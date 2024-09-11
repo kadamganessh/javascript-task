611f9a51cf4c669663139b0beb1b116f28a2f3e3
kadamganessh <142419164+kadamganessh@users.noreply.github.com>
2024-09-11 12:32:02 +0530

    Add files via upload

---
+++ script_10.js
@@ -0,0 +1,35 @@
+
+// operetors :
+// 1 arthmetic operetors 
+// console.log("add:",10+5);
+// console.log("Sub:",10-5);
+// console.log("add:",10*5);
+// console.log("add:",10/5);
+// console.log("add:",10%5);
+// console.log("add:",10**5);
+
+
+// 2 comparison operetors
+
+// console.log(3==5);
+// console.log(3!=5);
+// console.log(3>5);
+// console.log(3<5);
+// console.log(3>=5);
+// console.log(3<=5);
+// console.log(3==5);
+// console.log(3===5);
+// console.log(3!==5);
+
+
+// 3 Logical operetors
+
+// console.log(3<1 && 4<8); 
+
+// console.log(3<1 || 4==5);
+// console.log(! true);
+ 
+//  bitwise operetors
+
+console.log(~5);
+
---
+++ script_11sep.js
@@ -0,0 +1,66 @@
+// ex.1
+// Even odd :
+// var num = 9;
+// if (num % 2 == 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+
+
+// var num =55 ;
+// if(num > 10 && num <20){
+//     console.log("accepted");
+// } 
+// else{
+//     console.log("Rejected");
+// }
+
+
+// Ex.3
+
+// var year = 2003;
+// if (year >= 1500) {
+//     if(year%4==0){
+//     console.log("Leap year");
+//     }
+//     else {
+//     console.log("It is not leap year");
+//     }
+// }
+// else {
+//     console.log("INvalid entry");
+// }
+
+
+// Ex.4
+
+// var marks = 85;
+// if(marks>=80){
+//     console.log("Grade A");
+// }
+// else if(marks >=60 && marks <=79){
+//     console.log("grade B");
+// }
+// else if (marks >=35 && marks>=59){
+//     console.log("grade C");
+// }
+// else{
+//     console.log("fail");
+// }
+
+// Ex.5
+
+// var char ='A' ;
+// char=char.toLowerCase()
+// if(char == "a" || char == "e" || char=="o" || char =="u"|| char=="i"){
+//     console.log(char +  "  is Vowel");
+// }
+// else{
+//     console.log(char + "  is not Vowel");
+// }
+
+var num = 10;
+var result = num==0 ? "Zero" : num > 0 ? "Positiv" : "Negativ";
+console.log(result);
